49b14055755d258fbb92670743386e477fbe0977
Kylian LECHERE <k.lechere@estia.local>
2023-03-06 16:10:40 +0100

    timer

--- main.cpp
+++ main.cpp
@@ -7,18 +7,37 @@
 namespace {
 #define PERIOD_MS 2000ms
 DigitalOut myled(LED1);
+InterruptIn bouton(BUTTON1);
+uint64_t a = 0;
+int b = 0;
+Timer t;
+}
+void flip(){
+	t.start();
+	myled.write(1);
+}
+
+void flip2(){
+	t.stop();
+	a=t.read_ms();
+	b=1;
 }
 
 int main()
 {
-	while (true) {
-		printf("Alive! \n");
-		if (myled.read() == 0){
-			myled.write(1);
-		}
-		else {
+	bouton.rise(flip);
+	bouton.fall(flip2);
+	
+	while(true)	{
+
+		if (b == 1) {
+			printf("timer = %lld milliseconds\n",a);
 			myled.write(0);
+			b=0;
+			t.reset();
 		}
 		ThisThread::sleep_for(PERIOD_MS / 2);
 	}
+		
+	
 }
